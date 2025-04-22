6e245cd90af238c606921a70b1eba03c01602cb4
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-22 17:33:00 +0600

    do while

---
+++ src/DoWhileLoop/example1.java
@@ -0,0 +1,16 @@
+package DoWhileLoop;
+
+public class example1 {
+
+    public static void main(String[] args) {
+        int balance = 10;
+
+        do
+        {
+            System.out.println("Today balance add : " + balance);
+            balance++;
+        }
+        while (balance <= 15);
+
+    }
+}
