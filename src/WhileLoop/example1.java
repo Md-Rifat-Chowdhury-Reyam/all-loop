c21022f89f39e13b18b34657b3f2925f2f6407bc
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-22 16:40:19 +0600

    while loop

---
+++ src/WhileLoop/example1.java
@@ -0,0 +1,14 @@
+package WhileLoop;
+
+public class example1 {
+
+    public static void main(String[] args) {
+        int balance = 100; // initialized
+
+        while(balance <= 110 )
+        {
+            System.out.print(balance + " ");
+            balance++;
+        }
+    }
+}
